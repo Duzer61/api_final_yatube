92f745776a97541f2882135fa57e0fb2e4cfffa9
Данил Кочетов <dmi194@yandex.ru>
2023-02-07 13:36:54 +0300

    Добавил пагинацию, модель Group, сериализатор, пермишен глобальный и для автора

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import CommentViewSet, PostViewSet
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+router.register('posts/(?P<post_id>\\d+)/comments',
+                CommentViewSet, basename='comments')
+router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,15 @@
-# TODO:  Напишите свой вариант
+from posts.models import Comment, Post, User
+from rest_framework import permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import CommentSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
